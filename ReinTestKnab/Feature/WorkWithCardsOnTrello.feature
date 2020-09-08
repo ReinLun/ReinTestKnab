e260125c41567f2cff06f87adaa93d17020efbbe
Rein <rein@westweb.nl>
2020-09-08 17:08:23 +0200

    Add project files.

---
+++ ReinTestKnab/Feature/WorkWithCardsOnTrello.feature
@@ -0,0 +1,13 @@
+﻿Feature: WorkWithCardsOnTrello
+	Create a card on a existing Trello Board
+	Read the new card on a existing Trello Board
+	Update the new card on a existing Trello Board
+	Delete the new card on a existing Trello Board
+
+@smoke
+Scenario: Tester Works with cards
+	Given the tester create a card
+	Then the tester reads the new card
+	Then the tester updates the new card
+	Then the tester delete the new card
+	And  the card does not exists anymore
---
+++ ReinTestKnab/Steps/WorkWithCardsOnTrelloSteps.cs
@@ -0,0 +1,44 @@
+﻿using TechTalk.SpecFlow;
+
+
+namespace ReinTestKnab.Steps
+{
+    [Binding]
+    public class WorkWithCardsOnTrelloSteps
+    {
+        public static string ApiKey = "Vul hier de APIKey in";
+        public static string Token =  "Vul hier het Token in";
+
+        [Given(@"the tester create a card")]
+        public void GivenTheTesterCreateACard()
+        {
+            TrelloConnect.CreateCard(ApiKey, Token, "Rein Test Knab", "Te doen", "Rein Kaart", "TestKaart");
+            
+        }
+        
+        [Then(@"the tester reads the new card")]
+        public void ThenTheTesterReadsTheNewCard()
+        {
+            TrelloConnect.ReadCard(ApiKey, Token);
+        }
+        
+        [Then(@"the tester updates the new card")]
+        public void ThenTheTesterUpdatesTheNewCard()
+        {
+            TrelloConnect.UpdateCard(ApiKey, Token, "Knab test Rein");
+        }
+        
+        [Then(@"the tester delete the new card")]
+        public void ThenTheTesterDeleteTheNewCard()
+        {
+            TrelloConnect.DeleteCard(ApiKey, Token);
+        }
+        
+        [Then(@"the card does not exists anymore")]
+        public void ThenTheTheCardDoesNotExistsAnymore()
+        {
+            TrelloConnect.ReadCardAgain(ApiKey, Token);
+        }
+
+    }
+}
